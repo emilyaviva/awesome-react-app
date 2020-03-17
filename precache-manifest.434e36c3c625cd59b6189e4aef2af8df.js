943a895782411ee6bafc998e0d7a776c35411084
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2020-03-17 13:28:36 -0700

    Updates

---
+++ precache-manifest.434e36c3c625cd59b6189e4aef2af8df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614f515837861e00cc6e5d8bfc0ab10e",
+    "url": "/awesome-react-app/index.html"
+  },
+  {
+    "revision": "a860c4457bab5d51fefe",
+    "url": "/awesome-react-app/static/css/main.e5a2a18c.chunk.css"
+  },
+  {
+    "revision": "e5f2413831a4cbe24662",
+    "url": "/awesome-react-app/static/js/2.534895c4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/awesome-react-app/static/js/2.534895c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a860c4457bab5d51fefe",
+    "url": "/awesome-react-app/static/js/main.b68de94b.chunk.js"
+  },
+  {
+    "revision": "e83c23ba960d6afb15aa",
+    "url": "/awesome-react-app/static/js/runtime-main.86d244f3.js"
+  }
+]);
